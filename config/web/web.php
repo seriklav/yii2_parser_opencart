a2b7948f2bcdd198f479ed149a3d07e9e14f8484
Serik Lavrinyuk <lavrinyuk.serik@gmail.com>
2019-03-16 19:15:22 +0200

    Connect Yii2!

---
+++ config/web/web.php
@@ -0,0 +1,7 @@
+<?php
+    return [
+        'id' => 'parser',
+        'basePath' => realpath(__DIR__ . '/../')
+    ];
+
+?>
---
+++ web/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: serik_lav
+ * Date: 2019-03-16
+ * Time: 17:46
+ */
+
+    require_once __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+    $config = require_once  __DIR__ . '/../config/web/web.php';
+    (new yii\web\Application($config))->run();
